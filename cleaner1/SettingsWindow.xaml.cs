d3a9771b08ab98eee4875374d26fc9fe50a44333
Naix <76706715+naix1337@users.noreply.github.com>
2025-05-22 14:16:28 +0200

    Add files via upload

---
+++ cleaner1/SettingsWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace ModernFileCleaner
+{
+    public partial class SettingsWindow : Window
+    {
+        private readonly MainWindow mainWindow;
+
+        public SettingsWindow(MainWindow owner)
+        {
+            InitializeComponent();
+            mainWindow = owner;
+            Owner = owner;
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
+            chkAutoAnalyze.IsChecked = mainWindow.AutoAnalyze;
+            chkAutoClean.IsChecked = mainWindow.AutoClean;
+            chkShowNotifications.IsChecked = mainWindow.ShowNotifications;
+        }
+
+        private void SaveSettings()
+        {
+            mainWindow.AutoAnalyze = chkAutoAnalyze.IsChecked ?? false;
+            mainWindow.AutoClean = chkAutoClean.IsChecked ?? false;
+            mainWindow.ShowNotifications = chkShowNotifications.IsChecked ?? false;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveSettings();
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
